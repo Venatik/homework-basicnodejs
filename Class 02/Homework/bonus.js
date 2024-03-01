8d0fa059531bc9cf00f27efb382833ffeadd74a7
Venatik <stefan.trajkovski008@gmail.com>
2024-03-01 11:20:51 +0100

    Homework Class 2

--- Class 02/Homework/bonus.js
+++ Class 02/Homework/bonus.js
@@ -1,5 +1,4 @@
-import fs from "fs";
-import { writeFile, appendFile, readFile, createPath } from "./module.js";
+import fs, { read } from "fs";
 
 const editTask = (oldText, newText) => {
     fs.readFile("homework.txt", "utf8", function (err, data) {
@@ -17,4 +16,70 @@ const editTask = (oldText, newText) => {
     });
 };
 
-export { editTask };
+export { editTask, editUser, deleteUser, addUser, readUsers };
+
+const readUsers = () => {
+    const usersFile = fs.readFileSync("users.json", "utf8");
+
+    console.log("Users", JSON.parse(usersFile));
+};
+
+const editUser = (number, object) => {
+    const usersFile = fs.readFileSync("users.json", "utf8");
+    const users = JSON.parse(usersFile);
+
+    const userIndex = users.findIndex((user) => user.id === number);
+    if (userIndex === -1) {
+        console.log("User not found");
+        return;
+    }
+
+    users[userIndex] = object;
+
+    const updatedUsers = JSON.stringify(users);
+    fs.writeFileSync("users.json", updatedUsers);
+
+    console.log("User edited successfully.");
+    readUsers();
+};
+
+// editUser(4, { id: 5, name: "John Doe", username: "johnDoe", email: "" });
+
+const deleteUser = (number) => {
+    const usersFile = fs.readFileSync("users.json", "utf8");
+    const users = JSON.parse(usersFile);
+
+    const userIndex = users.findIndex((user) => user.id === number);
+    if (userIndex === -1) {
+        console.log("User not found");
+        return;
+    }
+
+    users.splice(userIndex, 1);
+
+    const updatedUsers = JSON.stringify(users);
+    fs.writeFileSync("users.json", updatedUsers);
+
+    console.log("User deleted successfully.");
+    readUsers();
+};
+
+// deleteUser(4);
+
+const addUser = (user) => {
+    const usersFile = fs.readFileSync("users.json", "utf-8");
+    const users = JSON.parse(usersFile);
+
+    const lastUserId = users.length > 0 ? users[users.length - 1].id : 0;
+    user.id = lastUserId + 1;
+
+    users.push(user);
+
+    const updatedUsers = JSON.stringify(users);
+    fs.writeFileSync("users.json", updatedUsers);
+
+    console.log("User added successfully.");
+    readUsers();
+}
+
+// addUser({ name: "Vinicius", username: "Vini Jr", email: "" });
--- Class 02/Homework/index.js
+++ Class 02/Homework/index.js
@@ -1,8 +1,13 @@
 import { writeFile, appendFile, readFile, createPath } from "./module.js";
-import { editTask } from "./bonus.js";
+import { editTask, editUser, deleteUser, addUser, readUsers } from "./bonus.js";
 
 writeFile();
 appendFile();
 readFile();
 
 editTask("FINISHED!", "DONE!");
+
+// deleteUser(5);
+// addUser({ name: "Rodrygo", username: "rodrygo", email: "" });
+// editUser(4, { id: 5, name: "Lunin", username: "Andrej4o", email: "" });
+readUsers();
